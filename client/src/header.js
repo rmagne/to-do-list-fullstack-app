597aed5a27f6aaac24b09762941cad7524733075
Romifff <romain.magne@edhec.com>
2023-08-16 12:38:48 +0200

    first routing commit

---
+++ client/src/header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <header>
+            <nav className="navbar">
+                <a href="">Login</a>
+                <a href="">Register</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
