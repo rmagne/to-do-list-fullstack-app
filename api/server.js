104366dd34e60ffd09f636803488ff8a1955288b
Romifff <romain.magne@edhec.com>
2023-09-02 15:27:09 +0200

    Revert "cleaning"
    This reverts commit b736889e894234c8580f38eebc5840e5ff6ab940.

--- api/server.js
+++ api/server.js
@@ -69,6 +69,13 @@ app.post('/login', async (req, res) => {
     }
 });
 
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+});
 
 
 app.post('/logout', (req, res) => {
